c77ed42f55a0707d11217a824daf9b5bec5c62fd
joao-p-marques <joao.marques.contacto@gmail.com>
2019-05-27 10:09:25 +0100

    Worker as class

--- worker.py
+++ worker.py
@@ -3,32 +3,42 @@
 import logging
 import argparse
 import asyncio
-
+from Mapper import Mapper
+from Reducer import Reducer
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
 logger = logging.getLogger('worker') 
 
-async def tcp_echo_client(message, host, port, loop):
-    reader, writer = await asyncio.open_connection(host, port, loop=loop)
+class Worker():
 
-    print('Send: %r' % message)
-    writer.write(message.encode())
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        mapper = Mapper()
+        reducer = Reducer()
+        logger.debug('Worker connecting to %s:%d', self.host, self.port)
 
-    data = await reader.read(100)
-    print('Received: %r' % data.decode())
+    async def tcp_echo_client(self, message, host, port, loop):
+        reader, writer = await asyncio.open_connection(host, port, loop=loop)
 
-    print('Close the socket')
-    writer.close()
+        print('Send: %r' % message)
+        writer.write(message.encode())
+
+        data = await reader.read(100)
+        print('Received: %r' % data.decode())
+        return data.decode()
+
+        print('Close the socket')
+        writer.close()	
 
 def main(args):
-    logger.debug('Connecting to %s:%d', args.hostname, args.port)
-
+    worker = Worker(args.hostname, args.port)
+    
     message = 'Hello World'
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(tcp_echo_client(message, args.hostname, args.port, loop))
+    loop.run_until_complete(worker.tcp_echo_client(message, worker.host, worker.port, loop))
     loop.close()
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='MapReduce worker')
