e6cce772ec2d9ca4c7f5ec97913002ad8fc3be06
tom1k <tomascosta@ua.pt>
2019-06-04 19:34:21 +0100

    fixed regex, counting words now

--- Mapper.py
+++ Mapper.py
@@ -1,6 +1,7 @@
 import logging
 import re
 import locale
+import string
 from functools import cmp_to_key
 
 locale.setlocale(locale.LC_ALL, 'pt_PT.UTF-8')
@@ -8,22 +9,25 @@ locale.setlocale(locale.LC_ALL, 'pt_PT.UTF-8')
 class Mapper():
 	"""docstring for Mapper"""
 	def __init__(self):
-		logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
-		self.logger = logging.getLogger('mapper')
+	    logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',datefmt='%m-%d %H:%M:%S')
+	    self.logger = logging.getLogger('mapper')
 
 	def map(self,blob):
-		#Ver um algoritmo melhor que isto?
-		wordArray = []
-		arrayMapper = sorted(re.split("[\s*\d*',''.']+",blob), key=cmp_to_key(locale.strcoll))
+            table = str.maketrans(dict.fromkeys('0123456789'))
+            translator = str.maketrans('', '', string.punctuation)
 
-		for word in arrayMapper:
-			word = word.lower()
-			if word.isalpha():
-				wordArray.append((word,1))
+            translated = blob.translate(translator)
+            # remove digits
+            translated = translated.translate(table)
 
-		return wordArray
+            # remove line breaks
+            translated = translated.replace('\n',' ')
+			
+            wordArray = []
+            arrayMapper = sorted( re.split("\s",translated), key=cmp_to_key(locale.strcoll))
 
+            for word in arrayMapper:
+                if word.isalpha():
+                    wordArray.append((word.lower(),1))
 
-					
-
-		
+            return wordArray
