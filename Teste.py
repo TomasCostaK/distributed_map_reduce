af602cb4db93e890bf9f96b647cd3685aaedd472
tom1k <tomascosta@ua.pt>
2019-06-04 11:35:12 +0100

    made changes to test

--- Teste.py
+++ Teste.py
@@ -4,7 +4,7 @@ from Reducer import Reducer
 def main():
 	mapper = Mapper()
 	reducer = Reducer()
-	arrayMap = mapper.map("Esta frase frase frase")
+	arrayMap = mapper.map("Esta Frase fRASe frase")
 	arrayMap2 = mapper.map("Este cena\n. frase")
 	mapFinal = reducer.reduce(arrayMap,arrayMap2)
 	print(mapFinal)
